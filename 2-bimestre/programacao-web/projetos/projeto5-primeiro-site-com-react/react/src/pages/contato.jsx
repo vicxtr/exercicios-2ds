e3ac502576b239a396f3436ddfa29ec6daa55285
João Victor <joao.silva4011@aluno.ce.gov.br>
2026-04-29 17:34:21 +0000

    projeto concluido

---
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/react/src/pages/contato.jsx
@@ -0,0 +1,11 @@
+export default function Contato() {
+    return(
+        <div>
+            <h2>Contato</h2>
+            <h1>Email para contato:</h1>
+            <p>contato@site.com</p>
+            <h1>Telefone para contato:</h1>
+            <p>08543567281</p>
+        </div>
+    )
+}
---
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/react/src/pages/sobre.jsx
@@ -0,0 +1,14 @@
+export default function Sobre() {
+    return(
+        <div>
+            <h2>Sobre</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo temporibus sapiente sed, rerum asperiores veritatis
+                quo commodi nemo accusantium eius, laborum esse. Soluta facere
+                accusantium, minima autem sunt ex quis!
+            </p>
+            <p>Somos uma empresa focada em tecnologia.</p>
+        </div>
+    )
+}
